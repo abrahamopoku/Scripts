04666c4f1a1a72eb5e5f99fa3759a5db5ecb0ee0
abrahamopoku <52693152+abrahamopoku@users.noreply.github.com>
2019-09-19 14:59:58 +0200

    Add files via upload

---
+++ docker-cp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+###### First code is using docker cp to copy from contain into ost #####
+docker cp 360953525df3:bookstack-files-backup.tar.gz /home/abraham/wikidb
+docker cp 360953525df3:bookstack.backup.sql /home/abraham/wikidb
+
+
+####################Abraham Opoku #####################################################
